ecbeb3111ae772565869b4da6bf2991f61efa619
tonybarton <51802313+tonybarton@users.noreply.github.com>
2019-07-18 20:08:03 -0400

    Add files via upload

---
+++ scanner.py
@@ -0,0 +1,198 @@
+from scapy.all import *
+from scapy.layers.dot11 import RadioTap, Dot11
+import os, signal, sys, string
+from pyfiglet import Figlet
+import time
+
+#access_points = set()
+whitelisted_aps = []
+blacklisted_aps = []
+channel = 1
+
+def filter_beacon(p):
+    """ Filter WiFi beacon and probe response packets. """
+    return p.haslayer(Dot11Beacon)
+
+
+def aps_scan(pkt):
+    global channel
+    global whitelisted_aps
+    global blacklisted_aps
+    parsed_list = []
+    ap={}
+    neighborap={}
+    rougeap={}
+
+    if (channel > 13):
+        channel = 1
+    channel_hopper()
+    channel += 1
+
+    # access_points.add(pkt[Dot11].addr3)
+    ssid = pkt[Dot11].info
+    bssid = pkt[Dot11].addr3
+    signal_strength = pkt[RadioTap].dBm_AntSignal
+    if signal_strength >= -30:
+        sig_str = "Perfect"
+    elif signal_strength >= -50 and signal_strength < -30:
+        sig_str = "Excellent"
+    elif signal_strength >= -60 and signal_strength < -50:
+        sig_str = "Good"
+    elif signal_strength >= -67 and signal_strength < -60:
+        sig_str = "Fair"
+    elif signal_strength >= -70 and signal_strength < -67:
+        sig_str = "Not Good"
+    elif signal_strength >= -80 and signal_strength < -70:
+        sig_str = "Bad"
+    elif signal_strength >= -90 and signal_strength < -80:
+        sig_str = "Really Bad"
+    channel = str(int(ord(pkt[Dot11Elt:3].info)))
+    netstats = pkt[Dot11Beacon].network_stats()
+    crypto = '/'.join(netstats['crypto'])
+    ap.update({"ssid":ssid})
+    ap.update({"mac":bssid})
+    ap.update({"sig_str":sig_str})
+    ap.update({"channel":channel})
+    ap.update({"crypto":crypto})
+
+    if ap not in whitelisted_aps or blacklisted_aps:
+
+        #print("SSID-------BSSID-------")
+        print("\n" + ssid + " " + bssid + " " + sig_str+ " " + channel + " " + crypto)
+        print(" ")
+        print("1: Whitelist \n" +
+              "2: Blacklist \n")
+        selection = input("Whitelist(1) or Blacklist(2): ")
+
+        if selection == 1:
+            whitelisted_aps.append(ap)
+            parsed_list.append(ap)
+            print(bssid + " " + "on " + ssid + " " + "has been whitelisted")
+        elif selection == 2:
+            blacklisted_aps.append(ap)
+            parsed_list.append(ap)
+        else:
+            print("Not a valid selection")
+
+
+
+def monitor_scan(pkt):
+    global channel
+    global whitelisted_aps
+    global blacklisted_aps
+    ap={}
+
+    if (channel > 13):
+        channel = 1
+    channel_hopper()
+    channel += 1
+
+    # access_points.add(pkt[Dot11].addr3)
+    ssid = pkt[Dot11].info
+    bssid = pkt[Dot11].addr3
+    signal_strength = pkt[RadioTap].dBm_AntSignal
+    if signal_strength >= -30:
+        sig_str = "Perfect"
+    elif signal_strength >= -50 and signal_strength < -30:
+        sig_str = "Excellent"
+    elif signal_strength >= -60 and signal_strength < -50:
+        sig_str = "Good"
+    elif signal_strength >= -67 and signal_strength < -60:
+        sig_str = "Fair"
+    elif signal_strength >= -70 and signal_strength < -67:
+        sig_str = "Not Good"
+    elif signal_strength >= -80 and signal_strength < -70:
+        sig_str = "Bad"
+    elif signal_strength >= -90 and signal_strength < -80:
+        sig_str = "Really Bad"
+    channel = str(int(ord(pkt[Dot11Elt:3].info)))
+    netstats = pkt[Dot11Beacon].network_stats()
+    crypto = '/'.join(netstats['crypto'])
+    ap.update({"ssid": ssid})
+    ap.update({"mac": bssid})
+    ap.update({"sig_str": sig_str})
+    ap.update({"channel": channel})
+    ap.update({"crypto": crypto})
+
+    if (ap not in whitelisted_aps) or (ap in blacklisted_aps):
+        print("Rouge AP Detected!!!")
+        print(ssid + " " + bssid + " " + sig_str + " " + channel + " " + crypto)
+        selection = input("1. Whitelist 2. Blacklist")
+
+        if selection == 1:
+            whitelisted_aps.append(ap)
+        elif selection == 2:
+            blacklisted_aps.append(ap)
+        else:
+            print("Not a valid selection")
+
+    #print(whitelisted_aps)
+    #print(blacklisted_aps)
+
+
+def channel_hopper():
+    try:
+        os.system("sudo iw dev %s set channel %d" % (interface, channel))
+    except Exception, err:
+        #logs_api.errors_log(str(err))
+
+        pass
+
+
+def init_scan():
+    interface = raw_input("Interface: ")
+    print("Setting interface to monitor mode...")
+    os.system("ifconfig " + interface + " " + "down")
+    os.system("iwconfig " + interface + " " + "mode monitor")
+    os.system("ifconfig " + interface + " " + "up")
+    print(interface + " " + "set to monitor mode")
+    sniff(iface=interface, lfilter=filter_beacon, prn=aps_scan)
+    main_menu()
+
+def monitor():
+    global interface
+    sniff(iface=interface, lfilter=filter_beacon, prn=monitor_scan)
+
+
+def usage():
+    print("\nList of commands \n\n" +
+          "scan: Used to obtain initial whitelist \n" +
+          "monitor: Set RougeAPDetector to monitor mode \n" +
+          "help: Displays this menu \n" +
+          "exit: Exits the program \n")
+    main_menu()
+
+def main_menu():
+    #global whitelisted_aps
+    #global blacklisted_aps
+    menu_cmd = str(raw_input("RougeAPDetector> "))
+
+    if menu_cmd == "help":
+        usage()
+    elif menu_cmd == "scan":
+        init_scan()
+    elif menu_cmd == "monitor":
+        monitor()
+    elif menu_cmd == "show whitelist":
+        print(whitelisted_aps)
+        main_menu()
+    elif menu_cmd == "show blacklist":
+        print(blacklisted_aps)
+        main_menu()
+    elif menu_cmd == "exit":
+        exit()
+    else:
+        print("Not a valid selection")
+        main_menu()
+
+def intro():
+    f = Figlet(font='slant')
+    word = 'ROUGE AP DETECTOR'
+    print f.renderText(word)
+
+def exit():
+    sys.exit()
+
+intro()
+usage()
+main_menu()
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pip install pyfiglet
+pip install scapy
